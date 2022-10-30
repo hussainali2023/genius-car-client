43b2a3fd8ea14b8318f3a23b0fdc7fb57d166ced
hussain2023 <iamhussainali2@gmail.com>
2022-10-30 22:33:41 +0530

    added header and footer compononet style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import router from "./Router/Routes/Routes";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
